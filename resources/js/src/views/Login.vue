a14df788c091e9a3326389319267898f75c9ee64
Sokna Pich <soknait@gmail.com>
2025-08-25 15:33:16 +0700

    Fixed build

--- resources/js/src/views/Login.vue
+++ resources/js/src/views/Login.vue
@@ -9,7 +9,7 @@
 
                     <input v-model="data.username" id="username" type="text"
                         class="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
-                        placeholder="Username" required>
+                        placeholder="Email" required>
                 </div>
                 <div>
                     <label class="block text-sm font-medium text-gray-700 mb-1" for="password">Password</label>
--- resources/js/src/views/Register.vue
+++ resources/js/src/views/Register.vue
@@ -16,7 +16,7 @@
 
           <input v-model="data.email" id="username" type="text"
             class="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
-            placeholder="Username" required>
+            placeholder="Email" required>
         </div>
         <div>
           <label class="block text-sm font-medium text-gray-700 mb-1" for="password">Password</label>
