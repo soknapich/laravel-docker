7864d73c78decaf5ff3cc207b03f719b38fc40d4
Sokna Pich <soknait@gmail.com>
2025-08-25 15:32:01 +0700

    Fixed build

--- resources/js/src/views/Login.vue
+++ resources/js/src/views/Login.vue
@@ -5,7 +5,7 @@
 
             <form @submit.prevent="submit" class="space-y-4">
                 <div>
-                    <label class="block text-sm font-medium text-gray-700 mb-1" for="username">Username</label>
+                    <label class="block text-sm font-medium text-gray-700 mb-1" for="username">Email</label>
 
                     <input v-model="data.username" id="username" type="text"
                         class="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
